c15fc1d3aeb9fa418f8fbe1edc90148ba0e6b954
Samuel Des Cormiers <169071632+GoodSirSami@users.noreply.github.com>
2024-10-17 10:11:57 -0400

    First commit, étape 4 du lab en cours

---
+++ socketApi.js
@@ -0,0 +1,22 @@
+/**
+ * @author Samuel Des Cormiers
+ * @description This file contains the code for socketio.
+ * @date 2024-09-30
+ * @version 1.0
+ */
+
+var socket_io = require('socket.io');
+var io = socket_io();
+var socketApi = {};
+
+socketApi.io = io;
+
+io.on('connection', function(socket){
+    console.log('A user connected');
+});
+
+// socketApi.sendNotification = function() {
+//     io.sockets.emit('hello', {msg: 'Hello World!'});
+// }
+
+module.exports = socketApi;
