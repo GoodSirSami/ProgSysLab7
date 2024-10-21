d239b6efb4b5b0cbbcf0c6043e5887226a7e18b2
Samuel Des Cormiers <169071632+GoodSirSami@users.noreply.github.com>
2024-10-21 08:02:52 -0400

    ouais faut pas trop

--- routes/users.js
+++ routes/users.js
@@ -20,29 +20,29 @@ router.post('/', function(req, res, next) {
     let pass = req.body.password.replace(/"/g, '').replace(/'/g, '').replace(/;/g, '').replace(/`/g, '').replace(/%/g, '');
 
     // Création de la requête SQL
-    let querystring = 'SELECT * FROM Users WHERE login = "'+ log +'" AND password = "'+ pass +'";';
+    let querystring = 'SELECT * FROM Users WHERE BINARY login = "'+ log +'" AND BINARY password = "'+ pass +'";';
 
     // Exécution de la requête
     connection.query(querystring, function (err, rows, fields) {
-      if (err) {
-        //socket.emit('loginFailed', { message: 'Erreur de connexion à la base de données' });
-        return;
-      }
-
+      if (err) throw err;
       // Si on a des résultats, on envoie les données de l'utilisateur
       if (rows.length > 0) {
         switch (rows[0].level) {
           case 3:
-            //socket.emit('loginSuccess', {login: rows[0].login, password: rows[0].password, level: rows[0].level.toString(), texte: rows[0].texte});
+            querystring = 'SELECT * FROM Users;';
+            connection.query(querystring, function (err, adminrows, fields) {
+              if (err) throw err;
+              res.render('pages/level3', { title: 'Level 3', texte: rows[0].texte, login: rows[0].login, rows: adminrows });
+            });
             break;
           case 2:
-            res.render('pages/level2', { title: 'Bonjour Wobert', texte: rows[0].texte, login: rows[0].login });
+            res.render('pages/level2', { title: 'Level 2', texte: rows[0].texte, login: rows[0].login });
             break;
           case 1:
-            //socket.emit('loginSuccess', {login: rows[0].login, password: "~", level: "~", texte: rows[0].texte});
+            res.render('pages/level1', { title: 'Level 1', texte: rows[0].texte, login: rows[0].login });
             break;
           default:
-            res.render('pages/level0', { title: 'Bonjour Emryc', texte: rows[0].texte });
+            res.render('pages/level0', { title: 'Level 0', texte: rows[0].texte, login: rows[0].login });
             break;
         }
       } else {
@@ -56,23 +56,40 @@ router.post('/', function(req, res, next) {
 
 io.on('connection', function(socket){
   socket.on('changeTexte', function(data){
-    let texte = data.texte.replace(/"/g, '').replace(/'/g, '').replace(/;/g, '').replace(/`/g, '').replace(/%/g, '');
+    let texte = '';
+    if(data.texte)
+    {
+      texte = data.texte.replace(/"/g, '').replace(/'/g, '').replace(/;/g, '').replace(/`/g, '').replace(/%/g, '');
+      texte = texte.slice(0, 250);
+    }
     let querystring = 'UPDATE Users SET texte = "'+ texte +'" WHERE login = "'+ data.login +'";';
     connection.query(querystring, function (err, rows, fields) {
       if (err) throw err;
       querystring = 'SELECT texte FROM Users WHERE login = "'+ data.login +'";';
-        connection.query(querystring, function (err, rows, fields) {
-          if (err) throw err;
-          if(rows.length > 0)
-          {
-            socket.emit('textSuccess', { texte: rows[0].texte });
-            socket.broadcast.emit('textSuccess', { texte: rows[0].texte });
-          }
-        });
+      connection.query(querystring, function (err, rows, fields) {
+        if (err) throw err;
+        if(rows.length > 0)
+        {
+          socket.emit('textSuccess', { texte: rows[0].texte, login: data.login });
+          socket.broadcast.emit('textSuccess', { texte: rows[0].texte, login: data.login });
+        }
+      });
+    });
+  });
+  socket.on('changePassword', function(data){
+    let password = '';
+    if(data.password)
+    {
+      password = data.password.replace(/"/g, '').replace(/'/g, '').replace(/;/g, '').replace(/`/g, '').replace(/%/g, '');
+      password = password.slice(0, 250);
+    }
+    let querystring = 'UPDATE Users SET password = "'+ password +'" WHERE login = "'+ data.login +'";';
+    connection.query(querystring, function (err, rows, fields) {
+      if (err) throw err;
+      socket.emit('passwordSuccess', { login: data.login });
+      socket.broadcast.emit('passwordSuccess', { login: data.login });
     });
   });
 });
-
-
 
 module.exports = router;
